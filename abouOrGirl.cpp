4dcb3fad4567c28801217d3d51e22628ff33a6cb
mastershifu09 <84791533+mastershifu09@users.noreply.github.com>
2022-01-26 20:58:34 +0600

    Add files via upload

---
+++ abouOrGirl.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int count=0;
+    string name;
+    cin>>name;
+    sort(name.begin(),name.end());
+
+        for (int i=0;i<name.length();i++)
+        {
+            if(name[i]!=name[i+1]){
+                count++;
+            }
+        }
+//cout<<count<<endl;
+    if(count%2==0)
+    {
+        cout<<"CHAT WITH HER!"<<endl;
+    }
+    else
+    {
+        cout<<"IGNORE HIM!"<<endl;
+    }
+    return 0;
+}
